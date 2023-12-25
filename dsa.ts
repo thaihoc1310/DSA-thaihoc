28826a5f237fafdb19696d7f203687d7c5b2c2ff
thaihoc <thaihoc131005@gmail.com>
2023-12-26 00:45:55 +0700

    home work 1

---
+++ dsa.ts
@@ -0,0 +1,15 @@
+//bai 1
+console.log("hello world!");
+//bai 2
+let a : string = "hello world";
+console.log(a);
+//bai 3
+let b : string = "hello";
+let c : string = "world";
+let d : string = b + " "+ c;
+console.log(d);
+//bai 4
+const print_ = (input:string) => console.log(input)
+print_(d);
+
+
